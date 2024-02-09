3c234f027b026e15237b0af45889056b500f27a0
Kelty Tech <Kelvinlinnaeus@gmail.com>
2024-02-09 14:36:57 +0000

    Experience

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Navbar from "./components/Navbar/Navbar.js";
 import "./App.css"
 import Intro from "./components/Intro/Intro.jsx";
 import Services from "./components/Services/Services.jsx";
+import Experience from "./components/Experience/Experience.jsx";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Navbar />
       <Intro />
       <Services />
+      <Experience />
     </div>
   );
 }
